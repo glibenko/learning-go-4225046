82c6ac9de12d9917311f65f197d808c92d1e5f7e
glibenko <glybenko@gmail.com>
2025-07-02 09:46:25 +0400

    http, files

--- Ch06/06_01b/main.go
+++ Ch06/06_01b/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 	fmt.Println("Files")
+	fileName := "./test.txt"
+	file, err := os.Create(fileName)
+	defer file.Close()
+	checkError(err)
+	fmt.Println("File created:", fileName)
+	length, err := io.WriteString(file, "Hello, World!\n")
+	checkError(err)
+	fmt.Println("Wrote", length, "characters to file:", fileName)
+	data, err := os.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("File content:", string(data))
+	// content := make([]byte, 64)
+	// length, err = file2.Read(content)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
--- Ch06/06_02b/main.go
+++ Ch06/06_02b/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	req.Header.Set("User-Agent", "Go HTTP Client/1.0")
+
+	resp, err := client.Do(req)
+	checkError(err)
+	defer resp.Body.Close()
+	fmt.Println("Response status:", resp.Status)
+	fmt.Printf("Response type:%T\n", resp)
+
+	bytes, err := io.ReadAll(resp.Body)
+	checkError(err)
+	content := string(bytes)
+	fmt.Println("Response content:", content)
 }
 
-// func checkError(err error) {
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
